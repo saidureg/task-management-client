ad35288ee1de8f915d85a7eae470b016bff0655e
saidureg <saidureg@gmail.com>
2023-12-22 19:55:15 +0600

    fixed some problem

--- src/pages/Dashboard/Tasks/ToDoList/ToDoListCard.jsx
+++ src/pages/Dashboard/Tasks/ToDoList/ToDoListCard.jsx
@@ -1,6 +1,6 @@
 const ToDoListCard = () => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-xl my-2">
       <div className="card-body">
         <h2 className="card-title">Card title!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
